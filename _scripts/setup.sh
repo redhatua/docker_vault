b4469703a79e8b15ee7b1c00e296125e0536d4ce
redhat <redhat.ua@gmail.com>
2019-03-27 20:09:44 +0200

    Cleanup

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -7,7 +7,7 @@ export VAULT_ADDR=http://127.0.0.1:8200
 
 ## INIT VAULT
 echo "[*] Init vault..."
-vault init -address=${VAULT_ADDR} > ./_data/keys.txt
+vault operator init -address=${VAULT_ADDR} > ./_data/keys.txt
 export VAULT_TOKEN=$(grep 'Initial Root Token:' ./_data/keys.txt | awk '{print substr($NF, 1, length($NF)-1)}')
 
 ## UNSEAL VAULT
