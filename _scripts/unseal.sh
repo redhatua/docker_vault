2ca6f32859683ce4289494120c14230bbf8a0ca2
redhat <redhat.ua@gmail.com>
2019-03-27 20:30:02 +0200

    Fix unseal script

--- _scripts/unseal.sh
+++ _scripts/unseal.sh
@@ -7,6 +7,6 @@ export VAULT_ADDR=http://127.0.0.1:8200
 
 ## UNSEAL VAULT
 echo "[*] Unseal vault..."
-vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 1:' ./_data/keys.txt | awk '{print $NF}')
-vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 2:' ./_data/keys.txt | awk '{print $NF}')
-vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 3:' ./_data/keys.txt | awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Unseal Key 1:' ./_data/keys.txt | cut -d ' ' -f4)
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Unseal Key 2:' ./_data/keys.txt | cut -d ' ' -f4)
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Unseal Key 3:' ./_data/keys.txt | cut -d ' ' -f4)
