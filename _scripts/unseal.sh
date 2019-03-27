675381e354e3ec8a4430e4397d8c3699e0c0e47d
redhat <redhat.ua@gmail.com>
2019-03-27 20:24:38 +0200

    Fix unseal script

--- _scripts/unseal.sh
+++ _scripts/unseal.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
 ## CONFIG LOCAL ENV
 echo "[*] Config local environment..."
+shopt -s expand_aliases
 alias vault='docker-compose exec vault vault "$@"'
 export VAULT_ADDR=http://127.0.0.1:8200
 
 ## UNSEAL VAULT
 echo "[*] Unseal vault..."
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 1:' ./_data/keys.txt | awk '{print $NF}')
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 2:' ./_data/keys.txt | awk '{print $NF}')
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 3:' ./_data/keys.txt | awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 1:' ./_data/keys.txt | awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 2:' ./_data/keys.txt | awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 3:' ./_data/keys.txt | awk '{print $NF}')
